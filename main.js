1edd7e23c425380d5cc6323b5cf4dee175a6686c
Davi Alves Pedroso <96917888+ODaviPedroso@users.noreply.github.com>
2023-02-13 17:50:20 -0300

    atualiza main js

--- main.js
+++ main.js
@@ -6,50 +6,56 @@ let complemento = document.getElementById("complemento")
 let numerodomedidor = document.getElementById("numerodomedidor")
 let codConsumidor;
 
+
 //buscar  id pelo cpf
 
 function getConsumidor() {
-      fetch(`https://localhost:7230/Consumidor/Documento/${documento.value}`)
+  fetch(`https://localhost:7230/Consumidor/Documento/${documento.value}`)
 
-  .then(response => response.json())
-  .then(data => {
-    var apiData = JSON.stringify(data) ;
-    codConsumidor = data.cod_Consumidor
-    console.log(apiData)
-  })
-  .catch(error => console.error(error));
-    };
+    .then(response => response.json())
+    .then(data => {
+      var apiData = JSON.stringify(data);
+      codConsumidor = data.cod_Consumidor
+      console.log(apiData)
+    })
+    .catch(error => console.error(error));
+};
 
 function postUser() {
-      let request = fetch("https://localhost:7230/UC", {
-      method: 'POST',
-      headers: {
-          "Accept": "*//*",
-          "content-type": "application/json",
+  setTimeout(getConsumidor(), 0)
+    fetch(`https://localhost:7230/UC`, {
+    method: 'POST',
+    headers: {
+      "Accept": "*//*",
+      "content-type": "application/json",
 
-      },
-      body: JSON.stringify({
-      cod_Consumidor :codConsumidor,
-      num_medidor : numerodomedidor.value,
-      num_casa  : numeroCasa.value,
-      cep : cep.value,
-      logradouro : logradoro.value,
-      Bairro : bairro.value,
-      complemento : complemento.value 
-})
+    },
+    body: JSON.stringify({
+      cod_Consumidor: codConsumidor,
+      num_medidor: numerodomedidor.value,
+      num_casa: numeroCasa.value,
+      cep: cep.value,
+      logradouro: logradoro.value,
+      Bairro: bairro.value,
+      complemento: complemento.value
+    })
   })
   //request.then(re=>console.log(re))
-  request.then(function (response) {
-      if (response.status == 200) {
-          alert("Consumidor inserido");
+  .then(function (response) {
+    if (response.status == 200) {
+      alert("Consumidor inserido");
 
-      }
-    })
+    }
+  })
 }
 
-var button = document.getElementById("myButton");
-button.addEventListener("submit", function(e) {
-  getConsumidor()
-  postUser() 
-  e.preventDefault()
-}); 
+
+var form = document.querySelector("form");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //getConsumidor()
+  postUser();
+  // aqui você pode colocar o código que deseja executar ao enviar o formulário
+
+
+});
