f205359412ec4e3e4296253c59a8d8ac045c126a
Davi Alves Pedroso <96917888+ODaviPedroso@users.noreply.github.com>
2023-02-13 16:48:01 -0300

    atualiza js

---
+++ main.js
@@ -0,0 +1,55 @@
+let documento = document.getElementById("documento")
+let cep = document.getElementById("cep")
+let logradoro = document.getElementById("logradouro")
+let numeroCasa = document.getElementById("documento")
+let complemento = document.getElementById("complemento")
+let numerodomedidor = document.getElementById("numerodomedidor")
+let codConsumidor;
+
+//buscar  id pelo cpf
+
+function getConsumidor() {
+      fetch(`https://localhost:7230/Consumidor/Documento/${documento.value}`)
+
+  .then(response => response.json())
+  .then(data => {
+    var apiData = JSON.stringify(data) ;
+    codConsumidor = data.cod_Consumidor
+    console.log(apiData)
+  })
+  .catch(error => console.error(error));
+    };
+
+function postUser() {
+      let request = fetch("https://localhost:7230/UC", {
+      method: 'POST',
+      headers: {
+          "Accept": "*//*",
+          "content-type": "application/json",
+
+      },
+      body: JSON.stringify({
+      cod_Consumidor :codConsumidor,
+      num_medidor : numerodomedidor.value,
+      num_casa  : numeroCasa.value,
+      cep : cep.value,
+      logradouro : logradoro.value,
+      Bairro : bairro.value,
+      complemento : complemento.value 
+})
+  })
+  //request.then(re=>console.log(re))
+  request.then(function (response) {
+      if (response.status == 200) {
+          alert("Consumidor inserido");
+
+      }
+    })
+}
+
+var button = document.getElementById("myButton");
+button.addEventListener("submit", function(e) {
+  getConsumidor()
+  postUser() 
+  e.preventDefault()
+}); 
